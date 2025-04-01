0cc13eb57ca8ae1baab2c9c2c38c3b0295b502e9
BuiVuTan <toisesongthatvoichinhminh@gmail.com>
2025-04-01 09:44:20 +0700

    Thi GK

---
+++ BuiVuTan_fitCMS_Blog/src/main/java/com/example/buivutan_fitcms_blog/thiGK/ntu64132105/BuiVuTanFitCmsBlogApplication.java
@@ -0,0 +1,12 @@
+package com.example.buivutan_fitcms_blog.thiGK.ntu64132105;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BuiVuTanFitCmsBlogApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BuiVuTanFitCmsBlogApplication.class, args);
+    }
+}
--- BuiVuTan_fitCMS_Blog/src/main/java/com/example/buivutan_fitcms_blog/thiGK/ntu64132105/controller/DashboardController.java
+++ BuiVuTan_fitCMS_Blog/src/main/java/com/example/buivutan_fitcms_blog/thiGK/ntu64132105/controller/DashboardController.java
@@ -1,4 +1,20 @@
 package com.example.buivutan_fitcms_blog.thiGK.ntu64132105.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
 public class DashboardController {
+
+    @GetMapping("/")
+    public String dashboard(Model model) {
+        model.addAttribute("username", "Bùi Vũ Tân");
+        return "dashboard";
+    }
+
+    @GetMapping("/dashboard")
+    public String dashboardRedirect(Model model) {
+        return "redirect:/";
+    }
 }
